c62d709e6785ae2b827d1b6aedf057a92da196ac
cindrawan <cindrawan@outlook.com>
2026-02-10 20:47:55 -0800

    Add files via upload
    Adding comments on .js and removing unnecessary codes on .css

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
+//declares variable 'tabs' as all the buttons in the header section
 let tabs = document.querySelectorAll('[data-tab-target]')
 
 tabs.forEach(tab => {
     tab.addEventListener('click', () => {
+        //declares variable 'target'. tab.dataset.tabTarget is taken from one of the button attributes, data-tab-target
         let target = document.querySelector(tab.dataset.tabTarget)
         let contents = document.querySelectorAll('[data-tab-content]')
+        //removes all active attribute in the paragraph everytime the user clicks
         contents.forEach(content => {
             content.classList.remove('active')
         })
         tabs.forEach(tab => {
             tab.classList.remove('active')
         })
+        //when user clicks, add active attribute
         target.classList.add('active')
         tab.classList.add('active')
     })
